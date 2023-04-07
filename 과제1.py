f4f34009f0827614334d54da54c5d022ebe89eb8
gyu6172 <gyu6172@gmail.com>
2023-04-07 20:12:36 +0900

    0407-과제1 충돌&게임오버 텍스트 구현

--- 과제1.py
+++ 과제1.py
@@ -1,5 +1,5 @@
 from vpython import *
-import random
+import random as rand
 
 
 def on_keydown(event):
@@ -8,9 +8,9 @@ def on_keydown(event):
         airplane.pos.x -= 0.5
     elif (event.key == "right" and airplane.pos.x < 9):
         airplane.pos.x += 0.5
-    elif (event.key == "up" and airplane.pos.y > -9):
+    elif (event.key == "up" and airplane.pos.y < 9):
         airplane.pos.y += 0.5
-    elif (event.key == "down" and airplane.pos.y < 9):
+    elif (event.key == "down" and airplane.pos.y > -9):
         airplane.pos.y -= 0.5
 
 
@@ -18,18 +18,21 @@ bulletList = list()
 
 # 총알 추가
 for i in range(7):
-    bullet = sphere(pos=vec(random.randrange(0, 18)-9, 9, 0),
+    bullet = sphere(pos=vec(rand.uniform(-9, 9), 9, 0),
                     radius=0.5, color=color.red)
-    bullet.v = vec(0, random.randrange(0, 10)-15, 0)
+    bullet.v = vec(0, rand.uniform(-20, -10), 0)
     bulletList.append(bullet)
 
 # 비행기 추가
 airplane = box(pos=vec(0, -8, 0), size=vec(1, 1, 1))
 
-# scene = canvas(width=800, height=600)
-# scene.range = 10
-# scene.title = "Dodge Game"
+timeText = label(pos=vec(-9, 9, 0))
+gameoverText = label(color=color.red, height=30, box=False)
+
+scene.range = 10
+scene.title = "Dodge Game"
 scene.bind("keydown", on_keydown)
+scene = canvas(pos=vec(0, 0, 0), width=800, height=600)
 
 t = 0
 dt = 0.01
@@ -41,9 +44,12 @@ while(not gameover):
         bullet.pos += bullet.v*dt
         if(bullet.pos.y < -9):
             bullet.pos.y = 9
-            bullet.v = vec(0, random.randrange(0, 10)-15, 0)
-        if((bullet.pos.x-0.5 < airplane.pos.x and airplane.pos.x < bullet.pos.x+0.5) and (bullet.pos.y-0.5 < airplane.pos.y and airplane.pos.y < bullet.pos.y+0.5)):
+            bullet.pos.x = rand.uniform(-9, 9)
+            bullet.v = vec(0, rand.uniform(-20, -10), 0)
+        if(mag(bullet.pos - airplane.pos) <= 1):
             gameover = True
+            gameoverText.text = "Game Over!"
             continue
+    timeText.text = "{:.2f}".format(t)
 
     t += dt
