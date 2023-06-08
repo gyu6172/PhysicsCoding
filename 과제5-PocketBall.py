d05f1f5fde23950278b14e760671771827129bd4
gyu6172 <gyu6172@gmail.com>
2023-06-08 17:19:07 +0900

    0608-과제5 포켓볼 벽 생성

--- 과제5-PocketBall.py
+++ 과제5-PocketBall.py
@@ -21,7 +21,7 @@ def on_keydown(event):
         shoot.theta += 1
     elif (event.key == "right"):
         shoot.theta -= 1
-    elif (event.key == "up" and shoot.power < 0.8):
+    elif (event.key == "up" and shoot.power < 0.5):
         shoot.power += 0.01
     elif (event.key == "down" and shoot.power > 0):
         shoot.power -= 0.01
@@ -45,6 +45,8 @@ def collision(b1, b2, e):
         j = j/(1/b1.m+1/b2.m)
         b1.v = b1.v + j*n_hat/b1.m
         b2.v = b2.v - j*n_hat/b2.m
+        b1.v *= 0.9
+        b2.v *= 0.9
         # _Option: Postion correction
         # scene.waitfor('click')
         #print(b1.radius + b2.radius - dist)
@@ -61,19 +63,23 @@ def col_pool(ball, ground, e):
     if -ground.size.x/2 > ball.pos.x - ball.radius:
         ball.pos.x = -ground.size.x/2 + ball.radius
         ball.v.x = -e*ball.v.x
+        ball.v *= 0.9
         col_flag = True
     if ground.size.x/2 < ball.pos.x + ball.radius:
         ball.pos.x = ground.size.x/2 - ball.radius
         ball.v.x = -e*ball.v.x
+        ball.v *= 0.9
         col_flag = True
     # y-axis collision check
     if -ground.size.z/2 > ball.pos.z - ball.radius:
         ball.pos.z = -ground.size.z/2 + ball.radius
         ball.v.z = -e*ball.v.z
+        ball.v *= 0.9
         col_flag = True
     if ground.size.z/2 < ball.pos.z + ball.radius:
         ball.pos.z = ground.size.z/2 - ball.radius
         ball.v.z = -e*ball.v.z
+        ball.v *= 0.9
         col_flag = True
     return col_flag
 
@@ -87,12 +93,15 @@ def updatePos(ball, dt):
         ball.v = vec(0, 0, 0)
 
 
-#x_axis = arrow(pos=vec(0, 0, 0), axis=vec(5, 0, 0), shaftwidth=0.03, color=color.red)
-#y_axis = arrow(pos=vec(0, 0, 0), axis=vec(0, 5, 0),shaftwidth=0.03, color=color.green)
-#z_axis = arrow(pos=vec(0, 0, 0), axis=vec(0, 0, 5), shaftwidth=0.03, color=color.blue)
+# x_axis = arrow(pos=vec(0, 0, 0), axis=vec(5, 0, 0),
+#                shaftwidth=0.03, color=color.red)
+# y_axis = arrow(pos=vec(0, 0, 0), axis=vec(0, 5, 0),
+#                shaftwidth=0.03, color=color.green)
+# z_axis = arrow(pos=vec(0, 0, 0), axis=vec(0, 0, 5),
+#                shaftwidth=0.03, color=color.blue)
 
 r = 0.0285
-m = 0.17
+m = 0.017
 
 ball_list = []
 color_list = [
@@ -117,7 +126,7 @@ initpos_list = []
 
 for i in range(5):
     for j in range(-i, i+1, 2):
-        initpos_list.append(vec(r*j, 0, -sqrt(3)*i*r*1.5))
+        initpos_list.append(vec(r*j, 0, -sqrt(3)*i*r-0.3))
 
 initpos = vec(0, 0, 0.8)
 ball_list.append(initBall(initpos, r, m, color.white))
@@ -127,7 +136,21 @@ for i in range(1, 16):
 
 w = 1.27
 h = 2.54
-table = box(pos=vec(0, -r-0.05, 0.5), size=vec(w, 0.1, h), color=color.green)
+table = box(pos=vec(0, -r-0.05, 0), size=vec(w, 0.1, h), color=color.green)
+
+hole_r = 0.05
+pw = w-2*hole_r
+ph = 0.1
+pt = 0.001
+
+plate1 = box(pos=vec(0, 0, -h/2), size=vec(pw, ph, pt), color=color.green)
+plate2 = box(pos=vec(0, 0, h/2), size=vec(pw, ph, pt), color=color.green)
+
+plate3 = box(pos=vec(w/2, 0, h/4), size=vec(pt, ph, pw), color=color.green)
+plate4 = box(pos=vec(w/2, 0, -h/4), size=vec(pt, ph, pw), color=color.green)
+plate5 = box(pos=vec(-w/2, 0, h/4), size=vec(pt, ph, pw), color=color.green)
+plate6 = box(pos=vec(-w/2, 0, -h/4), size=vec(pt, ph, pw), color=color.green)
+
 
 shoot = arrow(pos=ball_list[0].pos, color=color.white, shaftwidth=0.005)
 shoot.power = 0.3
@@ -135,19 +158,20 @@ shoot.theta = -180
 shoot.axis = vec(shoot.power*sin(radians(shoot.theta)),
                  0, shoot.power*cos(radians(shoot.theta)))
 
-uk = 0.1
+uk = 0.02
 g = vec(0, -9.8, 0)
 
-gamestate = {'ready': True, 'shoot': False}
+gamestate = {'ready': True, 'shoot': False, 'gameover': False}
 
 scene.bind("keydown", on_keydown)
 
 t = 0
 dt = 0.001
-while True:
+while not gamestate['gameover']:
     rate(1/dt)
 
     if(gamestate['ready']):
+        scene.center = ball_list[0].pos
         shoot.pos = ball_list[0].pos
         shoot.visible = True
         shoot.axis = vec(shoot.power*sin(radians(shoot.theta)),
